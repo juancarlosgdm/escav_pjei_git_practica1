987b75089a425d692d96fd9cb1f4d15d4fac064c
juancarlosgdm <juancarlosgdm@gmail.com>
2020-10-08 11:23:57 +0200

    Proyecto listo para práctica

---
+++ Assets/PracticaGit/PracticaGitBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PracticaGitBehaviour : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Introducir nombre del autor")]
+    private string nombreAutor;
+    
+    private void Start() {
+        Debug.Log(nombreAutor);
+    }
+}
