f3815949a77cc6881e91b86c21daf4a454db6fc6
Daniel <Dasarts73@gmail.com>
2020-10-20 10:31:39 +0200

    5.B

--- Assets/PracticaGit/PracticaGitBehaviour.cs
+++ Assets/PracticaGit/PracticaGitBehaviour.cs
@@ -6,6 +6,7 @@ public class PracticaGitBehaviour : MonoBehaviour {
     private string nombreAutor;
     
     private void Start() {
+        Debug.Log("Autor de la practica: ");
         Debug.Log(nombreAutor);
     }
 }
